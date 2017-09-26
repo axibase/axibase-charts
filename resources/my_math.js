2bd84ebaaee2b6bd8ad881ceb8c6206c1583ca63
Sergei Rodionov <sergei.rodionov@axibase.com>
2017-09-26 13:28:22 +0300

    fix my_math descr

--- resources/my_math.js
+++ resources/my_math.js
@@ -1,9 +1,10 @@
 /*
- This function multiple all values in the original series, identified by alias, by a constant numeric value.
+ This function multiplies values in the original series, identified by alias, by a constant numeric value.
 */
 exports.multiplyBy = function (alias, num) {
-  // multiple current value from the referenced series by 'num'
+  // multiply current value in the referenced series by 'num'
   var result = value(alias) * num;
-  // return the product to the outer function
+  // return the product to the calling function
   return result;
 };
+
